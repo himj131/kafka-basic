8f30fc105fc2c5571cbb1d01b183a2898422586e
himj131@gmail.com <himj131@gmail.com>
2020-10-18 17:28:29 +0900

    ConsumerConfig 적용

--- src/main/java/mj/kafka/consumer/MJKafkaConsumer.java
+++ src/main/java/mj/kafka/consumer/MJKafkaConsumer.java
@@ -1,5 +1,6 @@
 package mj.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -12,12 +13,12 @@ import java.util.Properties;
 public class MJKafkaConsumer {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "consumer-group");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.offset.reset", "latest");
-        props.put("key.deserializer", StringDeserializer.class);
-        props.put("value.deserializer", StringDeserializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList("mjtest"));
